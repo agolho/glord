0289bd8e97c0d07b738d14e118d67c9b945c253d
Yusuf Bektas <agolho@gmail.com>
2017-03-24 09:07:50 +0300

    Managers 2-6 added. Reset game adjustments added.

--- res/engine.js
+++ res/engine.js
@@ -1,14 +1,14 @@
-var money,item1,item2,item3,item4,item5,item6,manager1,manager2,manager3 = new Number();
+var money,item1,item2,item3,item4,item5,item6,manager1,manager2,manager3,manager4,manager5,manager6 = new Number();
 var Data = localStorage.getItem("glord-data");
 //These all were dutluk, next time they will be using something delicious nomoomonomonom!
 //Kookiies
 if (Data == null){
 
-  money=0,item1=10,item2=0,item3=0,item4=0,item5=0,item6=0,manager1=0,manager2=0,manager3=0;
+  money=0,item1=10,item2=0,item3=0,item4=0,item5=0,item6=0,manager1=0,manager2=0,manager3=0,manager4=0,manager5=0,manager6=0;
   localStorage.setItem("glord-data",[[money,item1,item2,item3,manager1,manager2,manager3]]);
 } else {
   var arrayed = Data.split(',');
-  money=Math.round(arrayed[0]),item1=Math.round(arrayed[1]),item2 = Math.round(arrayed[2]),item3=Math.round(arrayed[3]),manager1=Math.round(arrayed[4]),manager2=Math.round(arrayed[5]),manager3=Math.round(arrayed[6]),item4=Math.round(arrayed[7]),item5 = Math.round(arrayed[8]),item6=Math.round(arrayed[9]);
+  money=Math.round(arrayed[0]),item1=Math.round(arrayed[1]),item2 = Math.round(arrayed[2]),item3=Math.round(arrayed[3]),manager1=Math.round(arrayed[4]),manager2=Math.round(arrayed[5]),manager3=Math.round(arrayed[6]),item4=Math.round(arrayed[7]),item5 = Math.round(arrayed[8]),item6=Math.round(arrayed[9]),manager4=Math.round(arrayed[10]),manager5=Math.round(arrayed[11]),manager6=Math.round(arrayed[12]);
 }
 //Initializing
 setInterval(numbermaster5k, 1000);
@@ -16,17 +16,22 @@ setInterval(purchaseable, 250);
 setInterval(autoSave, 1000);
 //Autosave
 function autoSave(){
-  localStorage.setItem("glord-data",[Math.round(money),Math.round(item1),Math.round(item2),Math.round(item3),Math.round(manager1),Math.round(manager2),Math.round(manager3),Math.round(item4),Math.round(item5),Math.round(item6)]);
+  localStorage.setItem("glord-data",[Math.round(money),Math.round(item1),Math.round(item2),Math.round(item3),Math.round(manager1),Math.round(manager2),Math.round(manager3),Math.round(item4),Math.round(item5),Math.round(item6),Math.round(manager4),Math.round(manager5),Math.round(manager6)]);
 }
 //Delete
 $("#deleteData").click(function(e){
   e.preventDefault();
   localStorage.removeItem("glord-data");
-  money=0,item1=10,item2=0,item3=0,item4=0,item5=0,item6=0,manager1=0,manager2=0,manager3=0;
+  money=0,item1=10,item2=0,item3=0,item4=0,item5=0,item6=0,manager1=0,manager2=0,manager3=0,manager4=0,manager5=0,manager6=0;
 });
+//Dev Settings
 $("#newItemReset").click(function(e){
   e.preventDefault();
-  item4=0,item5=0,item6=0;
+  item4=0,item5=0,item6=0,manager2=0,manager3=0,manager4=0,manager5=0,manager6=0;
+});
+$("#addFunds").click(function(e){
+  e.preventDefault();
+  money+=1000000;
 });
 //Manual Churning
 $("#addonebtn").click(function(e){
@@ -50,7 +55,13 @@ function writer(){
   $("#item4").html('<i class="fa fa-bolt"></i> '+item4);
   $("#item5").html('<i class="fa fa-bomb"></i> '+item5);
   $("#item6").html('<i class="fa fa-eye"></i> '+item6);
-  if(manager1==1) $("#manager1").html('<i class="fa fa-certificate"></i>Çalışıyor Gevşek Süleyman 1hap/sn');
+  if(manager1==1) $("#manager1").html('<i class="fa fa-certificate"></i> Çalışıyor Gevşek Süleyman · 1hap/sn');
+  if(manager2==1) $("#manager2").html('<i class="fa fa-cube"></i> Çalışıyor Çakal Furkan · 1 küp/sn');
+  if(manager3==1) $("#manager3").html('<i class="fa fa-eercast"></i> Çalışıyor Dümen Yusuf · 1 torba/sn');
+  if(manager4==1) $("#manager4").html('<i class="fa fa-bolt"></i> Çalışıyor Arap Lawrence · 1 bolt/sn');
+  if(manager5==1) $("#manager5").html('<i class="fa fa-bomb"></i> Charles Ponzi · 1 bomba/sn');
+  if(manager6==1) $("#manager6").html('<i class="fa fa-eye"></i> Çalışıyor Sülün Osman · 1 nirvana/sn');
+
   var rounded=Math.round(money);
   if(rounded<999999){
     banksy('GEARS',rounded);
@@ -77,6 +88,12 @@ var item5sell = document.getElementById("item5sell");
 var item6buy = document.getElementById("item6buy");
 var item6sell = document.getElementById("item6sell");
 var managerbutton1 = document.getElementById("manager1");
+var managerbutton2 = document.getElementById("manager2");
+var managerbutton3 = document.getElementById("manager3");
+var managerbutton4 = document.getElementById("manager4");
+var managerbutton5 = document.getElementById("manager5");
+var managerbutton6 = document.getElementById("manager6");
+
 function purchaseable(){
   if (money>=100){
       item1buy.disabled = false;
@@ -150,13 +167,44 @@ function purchaseable(){
           else {
           item6sell.disabled = true;
           }
-  if (money>1000000){
-      managerbutton1.disabled = false;
-      }
-      else {
-      managerbutton1.disabled = true;
-      }
+if (money>1000000){
+    managerbutton1.disabled = false;
+    }
+    else {
+    managerbutton1.disabled = true;
+    }
+if (money>5000000){
+    managerbutton2.disabled = false;
+    }
+    else {
+    managerbutton2.disabled = true;
+    }
+if (money>10000000){
+    managerbutton3.disabled = false;
+    }
+    else {
+    managerbutton3.disabled = true;
+    }
+if (money>20000000){
+    managerbutton4.disabled = false;
+    }
+    else {
+    managerbutton4.disabled = true;
+    }
+if (money>40000000){
+    managerbutton5.disabled = false;
+    }
+    else {
+    managerbutton5.disabled = true;
+    }
+if (money>80000000){
+    managerbutton6.disabled = false;
+    }
+    else {
+    managerbutton6.disabled = true;
+    }
 }
+
 //Drug Purchase
 $("#item1sell").click(function(e){
   e.preventDefault();
@@ -257,11 +305,63 @@ $("#item6buy").click(function(e){
 //Manager Purchase
 $("#manager1").click(function(e){
   e.preventDefault();
-  if (money>=1000000){
-    manager1=1;
-    money-=1000000;
+  if(manager1=0){
+    if (money>=1000000){
+      manager1=1;
+      money-=1000000;
+    }
+    writer();
+  }
+});
+$("#manager2").click(function(e){
+  e.preventDefault();
+  if(manager2=0){
+  if (money>=5000000){
+    manager2=1;
+    money-=5000000;
   }
   writer();
+}
+});
+$("#manager3").click(function(e){
+  e.preventDefault();
+  if(manager3=0){
+  if (money>=10000000){
+    manager3=1;
+    money-=10000000;
+  }
+  writer();
+  }
+});
+$("#manager4").click(function(e){
+  e.preventDefault();
+  if(manager4=0){
+  if (money>=2000000){
+    manager4=1;
+    money-=2000000;
+  }
+  writer();
+}
+});
+$("#manager5").click(function(e){
+  e.preventDefault();
+  if(manager5=0){
+  if (money>=4000000){
+    manager5=1;
+    money-=4000000;
+  }
+  writer();
+}
+});
+$("#manager6").click(function(e){
+  e.preventDefault();
+  if(manager6=0){
+  if (money>=8000000){
+    manager6=1;
+    money-=8000000;
+  }
+  writer();
+}
 });
 //Patented Number Master 5000!
 function timing(){
@@ -272,11 +372,61 @@ function numbermaster5k(){
     if(item1>0) manager1work();
   }
   writer();
+  if(manager2==1){
+    if(item2>0) manager2work();
+  }
+  writer();
+  if(manager3==1){
+    if(item3>0) manager3work();
+  }
+  writer();
+  if(manager4==1){
+    if(item4>0) manager4work();
+  }
+  writer();
+  if(manager5==1){
+    if(item5>0) manager5work();
+  }
+  writer();
+  if(manager6==1){
+    if(item6>0) manager6work();
+  }
+  writer();
 }
 //Machines Producing
 function manager1work(){
   if (item1>0){
     item1--;
     money+=12;
+  }
+}
+function manager2work(){
+  if (item2>0){
+    item2--;
+    money+=120;
+  }
+}
+function manager3work(){
+  if (item3>0){
+    item3--;
+    money+=1300;
+  }
+}
+function manager4work(){
+  if (item4>0){
+    item4--;
+    money+=14000;
+  }
+}
+function manager5work(){
+  if (item5>0){
+    item5--;
+    money+=150000;
+  }
+}
+function manager6work(){
+  if (item6>0){
+    item6--;
+    money+=2100000;
   }
 }
